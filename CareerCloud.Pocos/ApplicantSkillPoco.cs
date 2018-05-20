f50e018a3b44ec7b846aaf3aeddb51ace01c1e70
Russ Gard <info@russgard.com>
2018-05-20 08:18:08 -0400

    Created all properties

--- CareerCloud.Pocos/ApplicantSkillPoco.cs
+++ CareerCloud.Pocos/ApplicantSkillPoco.cs
@@ -8,27 +8,21 @@ using System.Threading.Tasks;
 
 namespace CareerCloud.Pocos
 {
-    [Table("Applicant_Work_History")]
-    public class ApplicantWorkHistoryPoco
+    [Table("Applicant_Skills")]
+    public class ApplicantSkillPoco
     {
         [Key]
         public Guid Id { get; set; }
         public Guid Applicant { get; set; }
-        [Column("Company_Name")]
-        public string CompanyName { get; set; }
-        [Column("Country_Code")]
-        public string CountryCode { get; set; }
-        public string Location { get; set; }
-        [Column("Job_Title")]
-        public string JobTitle { get; set; }
-        [Column("Job_Description")]
-        public string JobDescription { get; set; }
+        public string Skill { get; set; }
+        [Column("Skill_Level")]
+        public string SkillLevel { get; set; }
         [Column("Start_Month")]
-        public Int16 StartMonth { get; set; }
+        public byte StartMonth { get; set; }
         [Column("Start_Year")]
         public int StartYear { get; set; }
         [Column("End_Month")]
-        public Int16 EndMonth { get; set; }
+        public byte EndMonth { get; set; }
         [Column("End_Year")]
         public int EndYear { get; set; }
         [Column("Time_Stamp")]
